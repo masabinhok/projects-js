605b7ccd31b4411e14520a38abe7f79415c44ae6
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-06-14 18:34:40 +0545

    filim ui

---
+++ filim/script.js
@@ -0,0 +1,10 @@
+//dom elements
+
+const sendButton = document.querySelector('.chat-send');
+
+
+
+//send-button interactive
+sendButton.addEventListener('click', () => {
+  console.log('hello');
+});
